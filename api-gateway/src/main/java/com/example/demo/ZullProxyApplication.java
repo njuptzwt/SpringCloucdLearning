a236f2f9f262a4e1493872467890037b2a951b3b
njupt <18362971301@163.com>
2018-01-29 13:37:35 +0800

    zull filter and route

--- api-gateway/src/main/java/com/example/demo/ZullProxyApplication.java
+++ api-gateway/src/main/java/com/example/demo/ZullProxyApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -11,5 +12,9 @@ public class ZullProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZullProxyApplication.class, args);
+	}
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
 	}
 }
